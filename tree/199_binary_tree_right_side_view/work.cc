0b870e43f3ad6b2e436c57186deb76da0cb12cb9
uangyy <uangyy@gmail.com>
2017-04-22 08:57:19 -0700

    add 199

---
+++ tree/199_binary_tree_right_side_view/work.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        if (!root)
+            return res;
+        res.push_back(root->val);
+        TreeNode *current = root->right;
+        if (!current)
+            return res;
+
+        queue<TreeNode *> level_node;
+        level_node.push(current);
+        int level_count;
+
+        while (!level_node.empty())
+        {
+            level_count = level_node.size();
+            while (level_count--)
+            {
+                current = level_node.front();
+                level_node.pop();
+                res.push_back(current->val);
+                if (current->left)
+                    level_node.push(current->left);
+                if (current->right)
+                    level_node.push(current->right);
+            }
+        }
+        return res;
+    }
+};
+
+int main(int argc, char **argv)
+{
+    TreeNode n1(1), n2(2), n3(3), n4(4), n5(5);
+    n1.left = &n2;
+    n1.right = &n3;
+    n2.right = &n5;
+    n3.right = &n4;
+    
+    Solution s;
+    vector<int> res;
+    res = s.rightSideView(&n1);
+
+    for (int i = 0; i < res.size(); ++i)
+    {
+        cout << res[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
